d8757efc4fed50225b4a6bec6f6330261373241a
Patrick Allan Wood <nuwudorder@gmail.com>
2021-02-12 10:23:51 -0800

    Update ballbot.py
    Had to add "ev3-ports:" (without quotes) before ev3devices/ in the path calling the ports.

--- ballbot.py
+++ ballbot.py
@@ -33,7 +33,7 @@ def FastWrite(outfile,value):
 # Open sensor files for (fast) reading
 touchSensorValueRaw = open("ev3devices/ev3-ports:in1/value0", "rb")
 gyroPitchRaw  = open("ev3devices/ev3-ports:in2/value0", "rb")
-gyroRollRaw  = open("ev3devices/ev3-ports:in3/value0", "rb")
+gyroRolllRaw  = open("ev3devices/ev3-ports:in3/value0", "rb")
 irSensor  = open("ev3devices/ev3-ports:in4/value0", "rb")
 
 # Initial state of the touch sensor
@@ -82,15 +82,15 @@ with open('ev3devices/ev3-ports:outD/polarity', 'w') as f:
     f.write('inversed')     
 
 # Open sensor files for (fast) reading
-motorEncoderRoll1 = open("ev3devices/ev3-ports:outA/position", "rb")   
+motorEncoderRolll1 = open("ev3devices/ev3-ports:outA/position", "rb")   
 motorEncoderPitch1 = open("ev3devices/ev3-ports:outB/position", "rb")   
-motorEncoderRoll2 = open("ev3devices/ev3-ports:outC/position", "rb")   
+motorEncoderRolll2 = open("ev3devices/ev3-ports:outC/position", "rb")   
 motorEncoderPitch2 = open("ev3devices/ev3-ports:outD/position", "rb")    
-
+     
 # Open motor files for (fast) writing
-motorDutyCycleRoll1 = open("ev3devices/ev3-ports:outA/duty_cycle_sp", "w")
+motorDutyCycleRolll1 = open("ev3devices/ev3-ports:outA/duty_cycle_sp", "w")
 motorDutyCyclePitch1 = open("ev3devices/ev3-ports:outB/duty_cycle_sp", "w")
-motorDutyCycleRoll2 = open("ev3devices/ev3-ports:outC/duty_cycle_sp", "w")
+motorDutyCycleRolll2 = open("ev3devices/ev3-ports:outC/duty_cycle_sp", "w")
 motorDutyCyclePitch2 = open("ev3devices/ev3-ports:outD/duty_cycle_sp", "w")
 
 # Function to set the duty cycle of the motors
@@ -130,9 +130,9 @@ def ResetEncoders():
 from imp import reload
 import parameters   
 from parameters import *  
-
+ 
 #Timing settings for the program
-loopTimeMiliSec         = 20                    # Time of each loop, measured in milliseconds.
+loopTimeMiliSec         = 20                    # Time of each loop, measured in miliseconds.
 loopTimeSec             = loopTimeMiliSec/1000  # Time of each loop, measured in seconds.
 motorAngleHistoryLength = 3                     # Number of previous motor angles we keep track of.
 
@@ -164,7 +164,7 @@ while True:
     # Wait for the Touch sensor to be bumped  
     WaitForTouchPress()
     WaitForTouchRelease()
-
+       
     # Reload tuning parameters from file (which may have been updated in the meantime)
     reload(parameters)
     from parameters import *        
@@ -175,7 +175,7 @@ while True:
     
     # A deque (a fifo array) which we'll use to keep track of previous motor positions, which we can use to calculate the rate of change (speed)
     motorAngleHistoryPitch = deque([0],motorAngleHistoryLength)
-    motorAngleHistoryRoll = deque([0],motorAngleHistoryLength)    
+    motorAngleHistoryRolll = deque([0],motorAngleHistoryLength)    
 
     # Variables representing physical signals
     motorAngleRawPitch              = 0 # The angle of "the motor", measured in raw units (degrees for the EV3). We will take the average of both motor positions as "the motor" angle, which is essentially how far the middle of the robot has traveled.
@@ -193,19 +193,19 @@ while True:
     gyroOffsetPitch                 = 0 # Over time, the gyro rate value can drift. This causes the sensor to think it is moving even when it is perfectly still. We keep track of this offset.
 
     # The same variables, but now for the roll direction
-    motorAngleRawRoll              = 0 # The angle of "the motor", measured in raw units (degrees for the EV3). We will take the average of both motor positions as "the motor" angle, which is essentially how far the middle of the robot has traveled.
-    motorAngleRoll                 = 0 # The angle of the motor, converted to radians (2*pi radians equals 360 degrees).
-    motorAngleReferenceRoll        = 0 # The reference angle of the motor. The robot will attempt to drive forward or backward, such that its measured position equals this reference (or close enough).
-    motorAngleErrorRoll            = 0 # The error: the deviation of the measured motor angle from the reference. The robot attempts to make this zero, by driving toward the reference.
-    motorAngleErrorAccumulatedRoll = 0 # We add up all of the motor angle error in time. If this value gets out of hand, we can use it to drive the robot back to the reference position a bit quicker.
-    motorAngularSpeedRoll          = 0 # The motor speed, estimated by how far the motor has turned in a given amount of time
-    motorAngularSpeedReferenceRoll = 0 # The reference speed during maneuvers: how fast we would like to drive, measured in radians per second.
-    motorAngularSpeedErrorRoll     = 0 # The error: the deviation of the motor speed from the reference speed.
-    motorDutyCycleRoll             = 0 # The 'voltage' signal we send to the motor. We calculate a new value each time, just right to keep the robot upright.
-    gyroRateRawRoll                = 0 # The raw value from the gyro sensor in rate mode.
-    gyroRateRoll                   = 0 # The angular rate of the robot (how fast it is falling forward or backward), measured in radians per second.
-    gyroEstimatedAngleRoll         = 0 # The gyro doesn't measure the angle of the robot, but we can estimate this angle by keeping track of the gyroRate value in time
-    gyroOffsetRoll                 = 0 # Over time, the gyro rate value can drift. This causes the sensor to think it is moving even when it is perfectly still. We keep track of this offset.
+    motorAngleRawRolll              = 0 # The angle of "the motor", measured in raw units (degrees for the EV3). We will take the average of both motor positions as "the motor" angle, which is essentially how far the middle of the robot has traveled.
+    motorAngleRolll                 = 0 # The angle of the motor, converted to radians (2*pi radians equals 360 degrees).
+    motorAngleReferenceRolll        = 0 # The reference angle of the motor. The robot will attempt to drive forward or backward, such that its measured position equals this reference (or close enough).
+    motorAngleErrorRolll            = 0 # The error: the deviation of the measured motor angle from the reference. The robot attempts to make this zero, by driving toward the reference.
+    motorAngleErrorAccumulatedRolll = 0 # We add up all of the motor angle error in time. If this value gets out of hand, we can use it to drive the robot back to the reference position a bit quicker.
+    motorAngularSpeedRolll          = 0 # The motor speed, estimated by how far the motor has turned in a given amount of time
+    motorAngularSpeedReferenceRolll = 0 # The reference speed during maneuvers: how fast we would like to drive, measured in radians per second.
+    motorAngularSpeedErrorRolll     = 0 # The error: the deviation of the motor speed from the reference speed.
+    motorDutyCycleRolll             = 0 # The 'voltage' signal we send to the motor. We calculate a new value each time, just right to keep the robot upright.
+    gyroRateRawRolll                = 0 # The raw value from the gyro sensor in rate mode.
+    gyroRateRolll                   = 0 # The angular rate of the robot (how fast it is falling forward or backward), measured in radians per second.
+    gyroEstimatedAngleRolll         = 0 # The gyro doesn't measure the angle of the robot, but we can estimate this angle by keeping track of the gyroRate value in time
+    gyroOffsetRolll                 = 0 # Over time, the gyro rate value can drift. This causes the sensor to think it is moving even when it is perfectly still. We keep track of this offset.
 
     # Counter of the main balancing loop
     loopCount = 0      
@@ -215,27 +215,27 @@ while True:
     ## Calibrate Gyro
     ##
     ########################################################################    
-        
-    
-    with open('/sys/class/power_supply/lego-ev3-battery/voltage_now','r') as f:
+          
+     
+    with open('/sys/class/power_supply/legoev3-battery/voltage_now','r') as f:
         voltage = f.read()
         print("Voltage: " + str(int(voltage)*0.000001))
-
+          
     print("-----------------------------------")      
     print("Calibrating...")
 
     #As you hold the robot still, determine the average sensor value of 100 samples
     gyroRateCalibrateCount = 100
     for i in range(gyroRateCalibrateCount):
-        gyroOffsetRoll = gyroOffsetRoll + FastRead(gyroRollRaw)
+        gyroOffsetRolll = gyroOffsetRolll + FastRead(gyroRolllRaw)
         gyroOffsetPitch = gyroOffsetPitch + FastRead(gyroPitchRaw)
         time.sleep(0.01)
     gyroOffsetPitch = gyroOffsetPitch/gyroRateCalibrateCount 
-    gyroOffsetRoll = gyroOffsetRoll/gyroRateCalibrateCount 
-
+    gyroOffsetRolll = gyroOffsetRolll/gyroRateCalibrateCount 
+           
     # Print the result   
     print("GyroOffsetPitch: ",gyroOffsetPitch)   
-    print("GyroOffsetRoll: ",gyroOffsetRoll) 
+    print("GyroOffsetRolll: ",gyroOffsetRolll) 
     time.sleep(0.1)
 
 
@@ -245,6 +245,7 @@ while True:
     ##
     ########################################################################    
 
+     
     # Time at start of loop
     tProgramStart = time.time()            
             
@@ -285,37 +286,37 @@ while True:
         ##  Reading the Gyro.
         ###############################################################
         gyroRateRawPitch = FastRead(gyroPitchRaw)
-        gyroRateRawRoll = FastRead(gyroRollRaw)
+        gyroRateRawRolll = FastRead(gyroRolllRaw)
         gyroRatePitch = (gyroRateRawPitch - gyroOffsetPitch)*radiansPerSecondPerRawGyroUnit
-        gyroRateRoll = (gyroRateRawRoll - gyroOffsetRoll)*radiansPerSecondPerRawGyroUnit
+        gyroRateRolll = (gyroRateRawRolll - gyroOffsetRolll)*radiansPerSecondPerRawGyroUnit
 
         ###############################################################
         ##  Reading the Motor Position
         ###############################################################
 
         motorAngleRawPitch = ((FastRead(motorEncoderPitch1) + FastRead(motorEncoderPitch2))/2)
-        motorAngleRawRoll = (FastRead(motorEncoderRoll1) + FastRead(motorEncoderRoll2))/2
+        motorAngleRawRolll = (FastRead(motorEncoderRolll1) + FastRead(motorEncoderRolll2))/2
         motorAnglePitch = motorAngleRawPitch*radiansPerRawMotorUnit
-        motorAngleRoll = motorAngleRawRoll*radiansPerRawMotorUnit
+        motorAngleRolll = motorAngleRawRolll*radiansPerRawMotorUnit
 
         motorAngularSpeedReferencePitch = forwardSpeedReference*radPerSecPerPercentSpeed
-        motorAngularSpeedReferenceRoll =    leftSpeedReference*radPerSecPerPercentSpeed
+        motorAngularSpeedReferenceRolll =    leftSpeedReference*radPerSecPerPercentSpeed
         motorAngleReferencePitch = motorAngleReferencePitch + motorAngularSpeedReferencePitch*loopTimeSec
-        motorAngleReferenceRoll = motorAngleReferenceRoll + motorAngularSpeedReferenceRoll*loopTimeSec
+        motorAngleReferenceRolll = motorAngleReferenceRolll + motorAngularSpeedReferenceRolll*loopTimeSec
 
         motorAngleErrorPitch = motorAnglePitch - motorAngleReferencePitch  
-        motorAngleErrorRoll = motorAngleRoll - motorAngleReferenceRoll   
+        motorAngleErrorRolll = motorAngleRolll - motorAngleReferenceRolll   
         
         ###############################################################
         ##  Computing Motor Speed
         ###############################################################
         
         motorAngularSpeedPitch = (motorAnglePitch - motorAngleHistoryPitch[0])/(motorAngleHistoryLength*loopTimeSec)
-        motorAngularSpeedRoll = (motorAngleRoll - motorAngleHistoryRoll[0])/(motorAngleHistoryLength*loopTimeSec)
+        motorAngularSpeedRolll = (motorAngleRolll - motorAngleHistoryRolll[0])/(motorAngleHistoryLength*loopTimeSec)
         motorAngularSpeedErrorPitch = motorAngularSpeedPitch - motorAngularSpeedReferencePitch
-        motorAngularSpeedErrorRoll = motorAngularSpeedRoll - motorAngularSpeedReferenceRoll
+        motorAngularSpeedErrorRolll = motorAngularSpeedRolll - motorAngularSpeedReferenceRolll
         motorAngleHistoryPitch.append(motorAnglePitch)
-        motorAngleHistoryRoll.append(motorAngleRoll)
+        motorAngleHistoryRolll.append(motorAngleRolll)
         
         
         #print motorAngularSpeedPitch, motorAngularSpeedReferencePitch, fwdSpeedControl
@@ -329,12 +330,12 @@ while True:
                                + gainMotorAngle * motorAngleErrorPitch
                                + gainMotorAngularSpeed * motorAngularSpeedErrorPitch
                                + gainMotorAngleErrorAccumulated * motorAngleErrorAccumulatedPitch)
-
-        motorDutyCycleRoll =   (gainGyroAngle  * gyroEstimatedAngleRoll
-                               + gainGyroRate   * gyroRateRoll
-                               + gainMotorAngle * motorAngleErrorRoll
-                               + gainMotorAngularSpeed * motorAngularSpeedErrorRoll
-                               + gainMotorAngleErrorAccumulated * motorAngleErrorAccumulatedRoll)
+                       
+        motorDutyCycleRolll =   (gainGyroAngle  * gyroEstimatedAngleRolll
+                               + gainGyroRate   * gyroRateRolll
+                               + gainMotorAngle * motorAngleErrorRolll
+                               + gainMotorAngularSpeed * motorAngularSpeedErrorRolll
+                               + gainMotorAngleErrorAccumulated * motorAngleErrorAccumulatedRolll)
         
         ###############################################################
         ##  Apply the signal to the motors, and add turning
@@ -342,24 +343,24 @@ while True:
 
         SetDuty(motorDutyCyclePitch1, motorDutyCyclePitch+turnRate)
         SetDuty(motorDutyCyclePitch2, motorDutyCyclePitch-turnRate)
-        SetDuty(motorDutyCycleRoll1, motorDutyCycleRoll+turnRate)
-        SetDuty(motorDutyCycleRoll2, motorDutyCycleRoll-turnRate)
+        SetDuty(motorDutyCycleRolll1, motorDutyCycleRolll+turnRate)
+        SetDuty(motorDutyCycleRolll2, motorDutyCycleRolll-turnRate)
 
         ###############################################################
         ##  Update angle estimate and Gyro Offset Estimate
         ###############################################################
 
         gyroEstimatedAnglePitch = gyroEstimatedAnglePitch + gyroRatePitch*loopTimeSec
-        gyroEstimatedAngleRoll = gyroEstimatedAngleRoll + gyroRateRoll*loopTimeSec
+        gyroEstimatedAngleRolll = gyroEstimatedAngleRolll + gyroRateRolll*loopTimeSec
         gyroOffsetPitch = (1-gyroDriftCompensationRate)*gyroOffsetPitch+gyroDriftCompensationRate*gyroRateRawPitch
-        gyroOffsetRoll = (1-gyroDriftCompensationRate)*gyroOffsetRoll+gyroDriftCompensationRate*gyroRateRawRoll
+        gyroOffsetRolll = (1-gyroDriftCompensationRate)*gyroOffsetRolll+gyroDriftCompensationRate*gyroRateRawRolll
 
         ###############################################################
         ##  Update Accumulated Motor Error
         ###############################################################
 
         motorAngleErrorAccumulatedPitch = motorAngleErrorAccumulatedPitch + motorAngleErrorPitch*loopTimeSec
-        motorAngleErrorAccumulatedRoll = motorAngleErrorAccumulatedRoll + motorAngleErrorRoll*loopTimeSec
+        motorAngleErrorAccumulatedRolll = motorAngleErrorAccumulatedRolll + motorAngleErrorRolll*loopTimeSec
 
         ###############################################################
         ##  Read the touch sensor (the kill switch)
@@ -370,7 +371,7 @@ while True:
         ###############################################################
         ##  Busy wait for the loop to complete
         ###############################################################
-
+       
         while(time.time() - tLoopStart <  loopTimeSec):
             time.sleep(0.0001) 
         
@@ -386,8 +387,8 @@ while True:
     # Turn off the motors    
     SetDuty(motorDutyCyclePitch1, 0)
     SetDuty(motorDutyCyclePitch2, 0)
-    SetDuty(motorDutyCycleRoll1, 0)
-    SetDuty(motorDutyCycleRoll2, 0)
+    SetDuty(motorDutyCycleRolll1, 0)
+    SetDuty(motorDutyCycleRolll2, 0)
 
     # Calculate loop time
     tLoop = (tProgramEnd - tProgramStart)/loopCount
